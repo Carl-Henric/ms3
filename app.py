48514eb6e2fbedb7de790f55bfb17f05340013cf
Carl-Henric <carlhenric.grip@gmail.com>
2021-03-15 09:58:17 +0000

    connected edit button to edit ad

--- app.py
+++ app.py
@@ -132,6 +132,14 @@ def add_ad():
     adGroups = mongo.db.adGroups.find().sort("adGroup_name", 1)
     return render_template("add_ad.html", adGroups=adGroups)
 
+
+@app.route("/edit_ad/<ad_id>", methods=["GET", "POST"])
+def edit_ad(ad_id):
+    ad = mongo.db.ads.find_one({"_id": ObjectId(ad_id)})
+    adGroups = mongo.db.adGroups.find().sort("adGroup_name", 1)
+    return render_template("edit_ad.html", ad=ad, adGroups=adGroups)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
